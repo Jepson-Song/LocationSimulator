d4df37d6b3d79cdb8a87626aeb071caeb9053f41
Jepson <1102982941@qq.com>
2020-01-01 18:27:14 +0800

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,340 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.SystemClock;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapPoi;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private MapView mMapView;
+    private BaiduMap mBaiduMap;
+    public LocationClient mLocationClient;
+    public BDLocationListener myListener = new MyLocationListener();
+    private Button bt;
+    private Button button;
+    private Button buttons;
+    private LatLng latLng;
+    private boolean isFirstLoc = true; // 是否首次定位
+    private LatLng choseLatLng;
+    private Marker mMarker;
+    private Thread thread;// 需要一个线程一直刷新
+    private Boolean RUN = true;
+    private LocationManager locationManager;
+    private String mMockProviderName = LocationManager.GPS_PROVIDER;;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+
+        //添加这下面的一部分
+        //动态申请权限
+        List<String> permissionList = new ArrayList<>();
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if(!permissionList.isEmpty()){
+            String[] permissions =permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+        }
+
+
+
+        initView();
+        initMap();
+        inilocation();
+
+        Toast.makeText(MainActivity.this, mMockProviderName, Toast.LENGTH_LONG).show();
+
+
+    }
+
+    /**
+     * inilocation 初始化 位置模拟
+     *
+     */
+    private void inilocation() {
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationManager.addTestProvider(mMockProviderName, false, true, false,
+                false, true, true, true, 0, 5);
+        locationManager.setTestProviderEnabled(mMockProviderName, true);
+        //locationManager.requestLocationUpdates(mMockProviderName, 0, 0, this);
+        //locationManager.requestLocationUpdates("gps", 1000L, 0.1F, this);
+
+    }
+
+    /**
+     * setLocation 设置GPS的位置
+     *
+     */
+    private void setLocation(double longitude, double latitude) {
+        Location location = new Location(mMockProviderName);
+        location.setTime(System.currentTimeMillis());
+        location.setLatitude(latitude);
+        location.setLongitude(longitude);
+        location.setAltitude(2.0f);
+        location.setAccuracy(3.0f);
+        if (Build.VERSION.SDK_INT > 16) {
+            //api 16以上的需要加上这一句才能模拟定位 , 也就是targetSdkVersion > 16
+            location.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
+        }
+        locationManager.setTestProviderLocation(mMockProviderName, location);
+    }
+
+
+    private void initMap() {
+        //获取地图控件引用
+        mBaiduMap = mMapView.getMap();
+        //普通地图
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        mBaiduMap.setMyLocationEnabled(true);
+
+        //默认显示普通地图
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        //开启交通图
+        //mBaiduMap.setTrafficEnabled(true);
+        //开启热力图
+        //mBaiduMap.setBaiduHeatMapEnabled(true);
+        // 开启定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+
+        mBaiduMap.setOnMapClickListener(new BaiduMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                changeMarker(latLng);
+            }
+            @Override
+            public void onMapPoiClick(MapPoi mapPoi)
+            {
+                changeMarker(mapPoi.getPosition());//POI点坐标
+            }
+        });
+
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        //配置定位SDK参数
+        initLocation();
+        mLocationClient.registerLocationListener(myListener);    //注册监听函数
+        //开启定位
+        mLocationClient.start();
+        //图片点击事件，回到定位点
+        mLocationClient.requestLocation();
+
+        //initOverlay();
+
+        setMarker();
+    }
+
+    /**
+     * 添加marker
+     */
+    private double latitude = 34;
+    private double longitude = 108;
+    private void setMarker() {
+        Log.v("pcw","setMarker : lat : "+ latitude+" lon : " + longitude);
+        //定义Maker坐标点
+        LatLng point = new LatLng(latitude, longitude);
+        //构建Marker图标
+        BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(R.drawable.icon_gcoding);
+        //构建MarkerOption，用于在地图上添加Marker
+        OverlayOptions option = new MarkerOptions().position(point).icon(bitmap);
+        //在地图上添加Marker，并显示
+        mMarker = (Marker) (mBaiduMap.addOverlay(option));
+    }
+
+    private void changeMarker(LatLng tmplatlang){
+        choseLatLng = tmplatlang;
+        mMarker.setPosition(choseLatLng);
+        latitude = tmplatlang.latitude;
+        longitude = tmplatlang.longitude;
+        Toast.makeText(MainActivity.this, "纬度："+latitude+"  经度："+longitude, Toast.LENGTH_SHORT).show();
+    }
+
+
+    //配置定位SDK参数
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span = 1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation
+        // .getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);
+        option.setOpenGps(true); // 打开gps
+
+        //可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+    }
+
+    //实现BDLocationListener接口,BDLocationListener为结果监听接口，异步获取定位结果
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+            // 构造定位数据
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(100).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            // 设置定位数据
+            mBaiduMap.setMyLocationData(locData);
+            // 当不需要定位图层时关闭定位图层
+            //mBaiduMap.setMyLocationEnabled(false);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(),
+                        location.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(18.0f);
+                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {
+                    // GPS定位结果
+                    Toast.makeText(MainActivity.this, location.getAddrStr(), Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                    // 网络定位结果
+                    Toast.makeText(MainActivity.this, location.getAddrStr(), Toast.LENGTH_SHORT).show();
+
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {
+                    // 离线定位结果
+                    Toast.makeText(MainActivity.this, location.getAddrStr(), Toast.LENGTH_SHORT).show();
+
+                } else if (location.getLocType() == BDLocation.TypeServerError) {
+                    Toast.makeText(MainActivity.this, "服务器错误，请检查", Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                    Toast.makeText(MainActivity.this, "网络错误，请检查", Toast.LENGTH_SHORT).show();
+                } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                    Toast.makeText(MainActivity.this, "手机模式错误，请检查是否飞行", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }
+    }
+
+    private void initView() {
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        bt = (Button) findViewById(R.id.bt);
+        bt.setOnClickListener(this);
+        button = (Button) findViewById(R.id.btMock);
+        button.setOnClickListener(this);
+        buttons = (Button) findViewById(R.id.buttons);
+        buttons.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+        thread = null;
+        //bitmap.recycle();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt:
+                //把定位点再次显现出来
+                MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.newLatLng(latLng);
+                mBaiduMap.animateMapStatus(mapStatusUpdate);
+                break;
+            case R.id.btMock:
+                //模拟地点
+                //mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+                // 开启线程，一直修改GPS坐标
+                if(thread!=null) thread = null;
+                thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (RUN) {
+                            try {
+                                Thread.sleep(50);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            setLocation(longitude, latitude);
+                        }
+                    }
+                });
+                thread.start();
+                Toast.makeText(MainActivity.this, "MOCK!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.buttons:
+                //普通地图
+                mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+                break;
+        }
+    }
+
+
+}
